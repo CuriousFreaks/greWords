ad97f80cf2353908a6f35c448bab7714e9b647f1
CuriousFreaks <thecuriousfreak007@gamail.com>
2018-02-22 01:28:41 +0530

    gasaini first commit

--- app/src/main/java/com/curiousfreaks/grewords/MainActivity.java
+++ app/src/main/java/com/curiousfreaks/grewords/MainActivity.java
@@ -2,12 +2,42 @@ package com.curiousfreaks.grewords;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button allWords, flashCards, myWordsList;
+        allWords=findViewById(R.id.allwords);
+        allWords.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(view.getContext(),"CLicked opening allWords",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        flashCards=findViewById(R.id.flashcards);
+        flashCards.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(view.getContext(),"CLicked opening flashCards",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        myWordsList=findViewById(R.id.mywordslist);
+        myWordsList.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(view.getContext(),"CLicked opening myWordsList",Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
