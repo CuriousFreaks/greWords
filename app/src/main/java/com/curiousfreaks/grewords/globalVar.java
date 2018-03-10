7e230ba92e9b878f47a896df2df64e7a91fbd984
CuriousFreaks <thecuriousfreak007@gamail.com>
2018-03-11 03:30:17 +0530

    updating UI

---
+++ app/src/main/java/com/curiousfreaks/grewords/globalVar.java
@@ -0,0 +1,12 @@
+package com.curiousfreaks.grewords;
+
+import android.os.Environment;
+
+/**
+ * Created by gasaini on 3/3/2018.
+ */
+
+public class globalVar {
+    public static final String jsonURL = "https://s3.ap-south-1.amazonaws.com/grewords/WordsList.json";
+    //public static final String jsonFile=getApplicationContext().getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS), "WordList.json";
+}
